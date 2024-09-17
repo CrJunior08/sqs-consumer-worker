5fd751a8743f3a5e173a36b00b42d6b55c05857f
CrJunior08 <cleristoncontato@outlook.com>
2024-09-17 08:58:27 -0300

    adicionado monitoramento da fila sqs e criado configuração docker

---
+++ sqs-consumer-worker/sqs-consumer-worker/2.Infrastructure/Messaging/SqsSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sqs_consumer_worker._2.Infrastructure.Messaging
+{
+    public class SqsSettings
+    {
+        public string QueueUrl { get; set; }
+    }
+}
--- sqs-consumer-worker/sqs-consumer-worker/Program.cs
+++ sqs-consumer-worker/sqs-consumer-worker/Program.cs
@@ -1,7 +1,23 @@
-using sqs_consumer_worker;
+using Amazon.SQS;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((hostContext, services) =>
+    {
+        // Configurar o AWS SQS
+        services.AddDefaultAWSOptions(hostContext.Configuration.GetAWSOptions());
+        services.AddAWSService<IAmazonSQS>();
 
-var host = builder.Build();
-host.Run();
+        // Registrar o worker que vai consumir as mensagens
+        services.AddHostedService<SqsConsumerWorker>();
+    })
+    .ConfigureLogging(logging =>
+    {
+        logging.ClearProviders();
+        logging.AddConsole();
+    })
+    .Build();
+
+await host.RunAsync();
